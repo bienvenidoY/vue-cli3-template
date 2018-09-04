f75ca1f5a84865301f3366093c4544196a11e36c
iyy <chengzhangiyy@163.com#macBookPro>
2018-09-04 15:49:52 +0800

    :tada: init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+const isPord = process.env.NODE_ENV === 'production'
+if(isPord) {
+  Vue.config.productionTip = false
+  Vue.config.devtools = false
+}
 
-Vue.config.productionTip = false
+console.log(process.env)
 
 new Vue({
   render: h => h(App)
